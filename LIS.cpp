d6a8802be0d03ef43a80e5e08fe629e1b6a8250e
_yummy_ <842720660@qq.com>
2017-09-24 22:01:28 +0800

    Create LIS.cpp

---
+++ LIS.cpp
@@ -0,0 +1,14 @@
+template<class _type>struct LIS{
+    static bool comp(const _type &a,const _type &b){
+        return a <= b;
+    }
+    ll getLisCon(_type *arr,ll len){
+        _type l[len],*_t,*_end = l;
+        for(ll i=0;i<len;i++){
+            _t = lower_bound(l,_end,arr[i],LIS::comp);
+            if(_t == _end)_end ++;
+            *_t = arr[i];
+        }
+        return _end - l;
+    }
+};
