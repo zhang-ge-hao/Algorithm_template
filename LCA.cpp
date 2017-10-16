0ea4a89294756fde85ed8801e96908f3a7c1f2fc
_yummy_ <842720660@qq.com>
2017-10-16 22:47:25 +0800

    up

---
+++ LCA.cpp
@@ -0,0 +1,45 @@
+struct LCA_rmq{
+    RMQ *r;
+    ll *n_ft,*t_n,*n_p,*n_r,*r_n,dfst,nid,maxn;
+    LCA_rmq(ll Maxn,ll maxl){
+        maxn = Maxn,r = new RMQ(maxl);
+        n_ft = new ll[maxn],n_p  = new ll[maxn];
+        n_r  = new ll[maxn],r_n  = new ll[maxn];
+        t_n  = new ll[maxl];
+    }
+    ~LCA_rmq(){
+        delete[] n_ft;
+        delete[] n_p;
+        delete[] n_r;
+        delete[] r_n;
+        delete[] t_n;
+        delete r;
+    }
+    void init(ll nodecon,const graph &T){
+        dfst = nid = 0;
+        memset(n_r,-1,sizeof(ll)*maxn);
+        for(ll i=1;i<=nodecon;i++)if(n_r[i] == -1){
+            dfs(i,-1,0,T);
+            t_n[dfst ++] = -1;
+        }
+        r->init(t_n,dfst);
+    }
+    void dfs(ll r,ll f,ll p,const graph &T){
+        ll *head = T.head; edge *E = T.E;
+        n_r[r] = nid,r_n[nid++] = r;
+        n_p[r] = p,n_ft[r] = dfst,t_n[dfst ++] = n_r[r];
+        for(ll i=head[r];i!=-1;i=E[i].next){
+            if(E[i].to == f)continue;
+            dfs(E[i].to,r,p+E[i].len,T);
+            t_n[dfst ++] = n_r[r];
+        }
+    }
+    ll find_path(ll a,ll b){
+        if(n_ft[a] > n_ft[b])swap(a,b);
+        ll pa = n_p[a],pb = n_p[b];
+        ll lca = r->find(n_ft[a],n_ft[b]+1);
+        if(lca == -1)return -1;
+        ll pc = n_p[r_n[lca]];
+        return pa+pb-2*pc;
+    }
+};
---
+++ RMQ.cpp
@@ -0,0 +1,28 @@
+struct RMQ{
+    ll **st,maxl,maxh; double log2;
+    RMQ(const ll &Maxl){
+        log2 = log(2);
+        maxl = Maxl;
+        maxh = log(maxl)/log2;
+        st = new ll*[maxh+1];
+        for(ll i=0;i<=maxh;i++)
+            st[i] = new ll[maxl+1-(1<<i)];
+    }
+    ~RMQ(){
+        for(ll i=0;i<=maxh;i++)delete[] st[i];
+        delete[] st;
+    }
+    void init(ll *arr,const ll &len){
+        ll col = log(len)/log2;
+        for(ll i=0;i<len;i++)st[0][i] = arr[i];
+        for(ll i=1;i<=col;i++)for(ll j=0;j<=len-(1<<i);j++)
+            st[i][j] = comp(st[i-1][j],st[i-1][j+(1<<(i-1))]);
+    }
+    ll find(ll lo,ll hi){ // zuo bi you kai
+        ll col = log(hi-lo)/log2;
+        return comp(st[col][lo],st[col][hi-(1<<col)]);
+    }
+    ll comp(ll a,ll b){
+        return a<b?a:b;
+    }
+};
